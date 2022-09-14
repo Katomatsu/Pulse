4843635579cc867c04ef30b65f87fbc44905c5d4
Adrokhman <adogairbekov@gmail.com>
2022-09-14 16:47:24 +0400

    create slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
-var name = 'Ivan';
-
-let number = 8;
-const pi = 3.14;
-
-let leftBorderWidth = 200;
-
-//number
-//string - "", '', ``
-//true/false
-//null
-//undefined`
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 500,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+    });
+});
